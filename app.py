f0d08862f85cdebce0c4e3c8335674ae4d51a55a
stemgene <hlj12530@gmail.com>
2023-05-09 22:36:08 -0400

    final

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import scipy.stats
 import time
+import pandas as pd
+
+# these are stateful variables which are preserved as Streamlin reruns this script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
 
 st.header('Tossing a Coin')
 
@@ -27,6 +35,16 @@ start_button = st.button("Run")
 
 if start_button:
     st.write(f'Running the experient of {number_of_trials} trials.')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['No.', 'iterations', 'mean'])
+    ],
+    axis=0)
+    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
 
-st.write('It is not a functional application yet. Under construction.') 
+st.write(st.session_state['df_experiment_results']) 
