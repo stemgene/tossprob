e36c2f3bc6f726f388164698c1d4b985a0cd693c
stemgene <hlj12530@gmail.com>
2023-05-12 08:52:44 -0400

    used car price

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# tossprob
+# Project Destription
+
+This project is a demo of Streamlit. I analyzed a table of used car's price and display some plots via Streamlit.
+
+The URL is https://tossprob.onrender.com/
+
+There is a data frame to show the details of data.
+
+There are two check-boxes: `Show Histograms` and `Show Scatters`. If click these check-boxes, the associated plots will display on the page.
+
+# Code explaination: `app.py`
+
+## Data Preprocess
+
+1. Clean data:
+    * Data type
+    * Missing values
+    * Duplicates
+    * Check outliers
+
+2. Enrich data:
+    * Break down the `model` into `make` and `model`
+
+## Analysis by plots
+
+### Histogram
+
+1. Vehicle types by manufacturer
+
+2. Histogram of `condition` vs `model_year`
+
+3. Compare price distribution between manufacturers
+
+### Scatters
+
+1. Price distribution by `model_year` and `model`
+
+# The project structure
+$ tree
+.
+├── README.md
+├── app.py
+├── vehicles_us.csv
+└── notebooks
+    └── EDA.ipynb
+└── .streamlit
+    └── config.toml 
--- app.py
+++ app.py
@@ -1,50 +1,86 @@
-import streamlit as st
-import scipy.stats
-import time
 import pandas as pd
+import streamlit as st
+import plotly_express as px
 
-# these are stateful variables which are preserved as Streamlin reruns this script
-if 'experiment_no' not in st.session_state:
-    st.session_state['experiment_no'] = 0
+df = pd.read_csv("vehicles_us.csv")
+# Preprocess
 
-if 'df_experiment_results' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
+# Proprocess -- Missing values
+df['model_year'] = df['model_year'].fillna(df['model_year'].mode()[0])
+df['cylinders'] = df['cylinders'].fillna(df['cylinders'].median())
+df['odometer'] = df['odometer'].fillna(df['odometer'].mean())
+df['paint_color'] = df['paint_color'].fillna(df['paint_color'].mode()[0])
+df['is_4wd'] = df['is_4wd'].fillna(0.0)
+# Preprocess -- extract manufacturer
+df['make'] = df['model'].apply(lambda x: x.split()[0])
+# Preprocess -- data type
+df['model_year'] = pd.to_datetime(df['model_year'].astype(int).astype(str), format='%Y').dt.year
+df['date_posted'] = pd.to_datetime(df['date_posted'])
+df['is_4wd'] = df['is_4wd'].apply(lambda x: 'yes' if x == 1.0 else 'no')
+# Preprocess -- duplicates
+def replace_wrong_values(df, col, replace_dict): 
+    for key, value in replace_dict.items(): 
+        df[col] = df[col].replace(key, value)
+        
+replace_dict = {"ford f150": 'ford f-150', 'ford f250': 'ford f-250', 'ford f250 super duty': 'ford f-250 super duty'}
+replace_wrong_values(df, 'model',  replace_dict)
+# Preprocess -- outliers
+df['price'] = df.loc[((df['price'] > 1000) & (df['price'] < 300000)), 'price'] 
+df['model_year'] = df.loc[(df['model_year'] > 1960), 'model_year']
+df['odometer'] = df.loc[((df['odometer'] < 700000) & (df['odometer'] > 10)), 'odometer']
 
-st.header('Tossing a Coin')
+# Streamlit part
+# Data table
+st.header('Data viewer')
+st.dataframe(df)
 
-chart = st.line_chart([0.5])
+# checkbox
+hist_plot = st.checkbox('Show Histograms')
+scatter_plot = st.checkbox("Show Scatters")
+# Histograms
+if hist_plot:
+    st.header("Vehicle types by manufacturer")
+    fig = px.histogram(df, x='make', color='type')
+    st.write(fig)
 
-def toss_coin(n):
-    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
-    
-    mean = None
-    outcome_no = 0
-    outcome_1_count = 0
-    
-    for r in trial_outcomes:
-        outcome_no += 1
-        if r == 1:
-            outcome_1_count += 1
-        mean = outcome_1_count / outcome_no
-        chart.add_rows([mean])
-        time.sleep(0.05)
-    return mean
+    st.header("Histogram of `condition` vs `model_year`")
+    fig = px.histogram(df, x='model_year', color='condition')
+    st.write(fig)
 
-number_of_trials =st.slider("Number of trials?", 1, 1000, 10)
-start_button = st.button("Run")
+    st.header("Compare price distribution between manufacturers")
+    manufact_list = sorted(df['make'].unique())
+    manufacturer_1 = st.selectbox(
+        label='Select manufacturer 1',
+        options=manufact_list,
+        index=manufact_list.index("chevrolet")
+    )
+    manufacturer_2 = st.selectbox(
+        label='Select manufacturer 2',
+        options=manufact_list,
+        index=manufact_list.index("hyundai")
+    )
 
-if start_button:
-    st.write(f'Running the experient of {number_of_trials} trials.')
-    st.session_state['experiment_no'] += 1
-    mean = toss_coin(number_of_trials)
-    st.session_state['df_experiment_results'] = pd.concat([
-        st.session_state['df_experiment_results'],
-        pd.DataFrame(data=[[st.session_state['experiment_no'],
-                            number_of_trials,
-                            mean]],
-                     columns=['No.', 'iterations', 'mean'])
-    ],
-    axis=0)
-    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
+    mask_filter = (df['make'] == manufacturer_1) | (df['make'] == manufacturer_2)
+    df_filtered = df[mask_filter]
 
-st.write(st.session_state['df_experiment_results']) 
+    normalize = st.checkbox("Normalize histogram", value=True)
+    if normalize:
+        histnorm = 'percent'
+    else:
+        histnorm = None
+
+    fig = px.histogram(df_filtered, x='price', nbins=30, color='make', histnorm=histnorm, barmode='overlay')
+    st.write(fig)
+
+# Scatter
+if scatter_plot:
+    st.header("Price distribution by `model_year` and `model`")
+    model_list = sorted(df['model'].unique())
+    model_selectbox = st.selectbox(
+        label='Select model',
+        options=model_list,
+        index=model_list.index(model_list[0])
+    )
+
+    fig = px.scatter(df[df['model'] == model_selectbox], x='model_year', y='price')
+    st.write(fig)
